ea5911fef6a141fb0b2ab894e1d77ba6808821c4
yisangho <yisangho717@gmail.com>
2019-06-13 16:45:25 -0500

    Implements Customer add after refresh

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -20,6 +20,7 @@ class CustomerAdd extends React.Component {
         this.addCustomer()
         .then((response) => {
             console.log(response.data);
+            this.props.stateRefresh();
         })
         this.setState({
             file: null,
@@ -29,7 +30,7 @@ class CustomerAdd extends React.Component {
             job: '',
             fileName: ''
         })
-        window.location.reload();
+        
     }
 
     handleFileChange = (e) => {

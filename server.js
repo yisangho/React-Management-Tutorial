9907ee0026f2bdec966b2b68687881f710d9ded5
yisangho <yisangho717@gmail.com>
2019-06-13 16:39:01 -0500

    Add customerAdd class

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,47 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/customers', (req, res) => {
-    res.send([
-            {
-                'id': 1,
-                'image': 'https://placeimg.com/64/64/1',
-                'name': 'Sangho',
-                'birthday': '19730714',
-                'gender': 'male',
-                'job': 'programmer'
-            },
-            {
-                'id': 2,
-                'image': 'https://placeimg.com/64/64/2',
-                'name': 'Sunmi',
-                'birthday': '19740101',
-                'gender': 'female',
-                'job': 'counselor'
-            },
-            {
-                'id': 3,
-                'image': 'https://placeimg.com/64/64/3',
-                'name': 'Kanghui',
-                'birthday': '20000223',
-                'gender': 'male',
-                'job': 'programmer'
-            }
-    ]);
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
 });
 
+connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
+app.get('/api/customers', (req, res) => {
+    connection.query(
+        "select * from customer",
+        (err, rows, fields) => {
+            res.send(rows);
+
+        }
+    )
+});
+
+app.use('/image', express.static('./upload'));
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'insert into customer (image, name, birthday, gender, job ) values (?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params, 
+        (err, rows, fields) => {
+            res.send(rows);
+            console.log(err);
+            console.log(rows);
+        })
+})
 app.listen(port, () => console.log(`Listening on port : ${port}`));
